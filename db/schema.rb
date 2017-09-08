74c1bc9979daf116b126eeb133e7641a4c1ee93b
Yuka Kato <satoimo.to.imo@gmail.com>
2017-09-08 17:34:05 +0900

    Add category_id column to work Rails association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170908071816) do
+ActiveRecord::Schema.define(version: 20170908082728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20170908071816) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_weapons_on_category_id"
   end
 
 end
